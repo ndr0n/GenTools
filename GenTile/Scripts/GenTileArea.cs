de1059196e1954f36051d54893e5de509117a4e5
ndr0n <esquilofreniko@gmail.com>
2025-09-25 16:52:17 +0100

    Updates

---
+++ GenTile/Scripts/GenTileArea.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GenTools;
+using Sentience;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Serialization;
+
+namespace MindTheatre
+{
+    [System.Serializable]
+    [CreateAssetMenu(fileName = "Area", menuName = "GenTools/Area")]
+    public class GenTileArea : ScriptableObject
+    {
+        public int Seed = 0;
+        public List<byte[,]> Map = null;
+        public Vector3Int WorldPosition = Vector3Int.zero;
+        public List<GenTilePreset> GenTilePresets = new();
+
+        #region Creation
+
+        public static GenTileArea CreateNewArea(int seed, string worldName, Vector3Int worldPosition)
+        {
+            GenTileArea area = CreateInstance<GenTileArea>();
+            area.Seed = seed;
+            area.name = worldName;
+            area.WorldPosition = new Vector3Int(worldPosition.x, worldPosition.y, 0);
+            SerializeArea(area, worldName, worldPosition);
+            return area;
+        }
+
+        public static GenTileArea CreateNewAreaFromTemplate(int seed, GenTileArea template, string worldName, Vector3Int worldPosition)
+        {
+            GenTileArea area = Instantiate(template);
+            area.Seed = seed;
+            area.name = worldName;
+            area.WorldPosition = new Vector3Int(worldPosition.x, worldPosition.y, 0);
+            GenTileArea.SerializeArea(area, worldName, worldPosition);
+            return area;
+        }
+
+        public static void SerializeArea(GenTileArea area, string worldName, Vector3Int worldPosition)
+        {
+#if UNITY_EDITOR
+            string path = AssetDatabase.GenerateUniqueAssetPath($"Assets/Modules/GenTools/GenTile/Data/World/{worldName}/{worldName}_WT_X{worldPosition.x}Y{worldPosition.y}Z{worldPosition.z}.asset");
+            AssetDatabase.CreateAsset(area, path);
+            AssetDatabase.SaveAssets();
+#endif
+        }
+
+        #endregion
+    }
+}
---
+++ GenTile/Scripts/GenTileWorld.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using System.Linq;
+using GenTools;
+using MindTheatre;
+using Sentience;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace GenTools
+{
+    [System.Serializable]
+    public class GenTileAreaTemplate
+    {
+        public Vector3Int MinPosition;
+        public Vector3Int MaxPosition;
+        public List<WorldTile> WorldAreaTemplate;
+    }
+
+    [System.Serializable]
+    [CreateAssetMenu(fileName = "World", menuName = "GenTools/World")]
+    public class GenTileWorld : ScriptableObject
+    {
+        public int WorldSeed = 0;
+        public List<WorldTile> Map = new();
+        public Vector3Int WorldSize = new Vector3Int(1, 1, 1);
+        public Vector3Int WorldAreaSize = new Vector3Int(50, 1, 50);
+        public List<GenTileAreaTemplate> WorldAreaTemplates = new();
+
+        public void Generate()
+        {
+            WorldSeed = Random.Range(int.MinValue, int.MaxValue);
+            System.Random random = new System.Random(WorldSeed);
+            Map = new();
+            for (int x = 0; x < WorldSize.x; x++)
+            {
+                for (int y = 0; y < WorldSize.y; y++)
+                {
+                    for (int z = 0; z < WorldSize.z; z++)
+                    {
+                        GenTileAreaTemplate areaTemplate = null;
+                        foreach (var template in WorldAreaTemplates.OrderBy(x => random.Next()))
+                        {
+                            if (x >= template.MinPosition.x && x <= template.MaxPosition.x && y >= template.MinPosition.y && y <= template.MaxPosition.y && z >= template.MinPosition.z && z <= template.MaxPosition.z)
+                            {
+                                areaTemplate = template;
+                                break;
+                            }
+                        }
+                        if (areaTemplate == null) areaTemplate = WorldAreaTemplates[random.Next(WorldAreaTemplates.Count)];
+                        int seed = random.Next(int.MinValue, int.MaxValue);
+                        WorldTile worldTile = WorldTile.CreateWorldTileFromTemplate(seed, areaTemplate.WorldAreaTemplate[random.Next(areaTemplate.WorldAreaTemplate.Count)], name, new Vector3Int(x, y, z));
+                        Map.Add(worldTile);
+                    }
+                }
+            }
+        }
+    }
+#if UNITY_EDITOR
+    [CustomEditor(typeof(GenTileWorld))]
+    public class GenTileWorld_Editor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            GenTileWorld world = (GenTileWorld) target;
+            if (GUILayout.Button("Generate"))
+            {
+                world.Generate();
+            }
+        }
+    }
+#endif
+}
